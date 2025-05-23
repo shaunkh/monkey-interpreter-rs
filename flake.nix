61387aebfae68cb0872dc734e55f5ed3cc34b36c
Shaun Khundker <shaun.khundker@gmail.com>
2025-05-23 14:11:18 -0400

    nix shell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A simple Rust dev shell";
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, fenix, ... }@inputs:
+    let
+      inherit (nixpkgs) lib;
+      forEachSystem = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in {
+      devShells = forEachSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ fenix.overlays.default ];
+          };
+        in {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [ pkgs.fenix.stable.toolchain ];
+          };
+        });
+    };
+}
